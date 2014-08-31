a3bbc8c8a79d9cf458d1f35f0052e474667ee601
Sarah GP <sarah.groff.palermo@gmail.com>
2014-08-31 15:16:09 -0400

    Node server

---
+++ server/scraper.js
@@ -0,0 +1,17 @@
+var fs = require('fs'),
+	request = require('request'),
+	exec = require('child_process').exec;
+
+function getTree () {
+	request.get("https://news.layervault.com/u/tree.json")
+			.pipe(fs.createWriteStream("tree.json")
+				.on('finish', function(){
+					exec('git commit -am "Auto tree update"', function(error){
+						if (!error){
+							exec('git push origin gh-pages');
+						}
+					});
+				}));
+}
+
+getTree();
